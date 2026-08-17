35af18790e5be6b61cc0a2834d4fa57958d89d13
thegirlbecoming-code <thegirlbecoming@gmail.com>
2026-08-17 15:46:42 -0400

    assets.js

---
+++ assets.js
@@ -0,0 +1,116 @@
+const Assets = {
+  skinTones: [
+    { id: "fair", color: "#F9DFC9" },
+    { id: "light", color: "#F2C7A5" },
+    { id: "medium", color: "#D89B6B" },
+    { id: "tan", color: "#B8754F" },
+    { id: "deep", color: "#7A4A2B" }
+  ],
+
+  hair: [
+    {
+      id: "long_brown",
+      name: "Long Brown",
+      back: "assets/hair/long_brown_back.png",
+      front: "assets/hair/long_brown_front.png"
+    },
+    {
+      id: "ponytail",
+      name: "Ponytail",
+      back: "assets/hair/ponytail_back.png",
+      front: "assets/hair/ponytail_front.png"
+    },
+    {
+      id: "braids",
+      name: "Braids",
+      back: "assets/hair/braids_back.png",
+      front: "assets/hair/braids_front.png"
+    }
+  ],
+
+  eyes: [
+    {
+      id: "soft",
+      name: "Soft Eyes",
+      image: "assets/eyes/soft.png"
+    },
+    {
+      id: "sparkle",
+      name: "Sparkle Eyes",
+      image: "assets/eyes/sparkle.png"
+    }
+  ],
+
+  noses: [
+    {
+      id: "small",
+      image: "assets/noses/small.png"
+    },
+    {
+      id: "button",
+      image: "assets/noses/button.png"
+    }
+  ],
+
+  mouths: [
+    {
+      id: "smile",
+      image: "assets/mouths/smile.png"
+    },
+    {
+      id: "openSmile",
+      image: "assets/mouths/open_smile.png"
+    }
+  ],
+
+  outfits: [
+    {
+      id: "coquette",
+      name: "Coquette",
+      image: "assets/outfits/coquette.png"
+    },
+    {
+      id: "school",
+      name: "School",
+      image: "assets/outfits/school.png"
+    },
+    {
+      id: "casual",
+      name: "Casual",
+      image: "assets/outfits/casual.png"
+    }
+  ],
+
+  accessories: [
+    {
+      id: "bow",
+      name: "Bow",
+      image: "assets/accessories/bow.png"
+    },
+    {
+      id: "glasses",
+      name: "Glasses",
+      image: "assets/accessories/glasses.png"
+    },
+    {
+      id: "flowers",
+      name: "Flowers",
+      image: "assets/accessories/flowers.png"
+    }
+  ],
+
+  backgrounds: [
+    {
+      id: "bedroom",
+      image: "assets/backgrounds/bedroom.png"
+    },
+    {
+      id: "garden",
+      image: "assets/backgrounds/garden.png"
+    },
+    {
+      id: "library",
+      image: "assets/backgrounds/library.png"
+    }
+  ]
+};
